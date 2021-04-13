5b6734d58038969fbf46a1be99a932eb6ce7987d
Md Ashraful Alam <45354597+alam2018@users.noreply.github.com>
2021-04-13 13:13:24 +0200

    Update launcher2.sh

--- launcher2.sh
+++ launcher2.sh
@@ -11,6 +11,6 @@ env
 iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
 cat /etc/srslte/enb.conf
 ./srsLTE/build/srsepc/src/srsepc &
-./srsenb/src/srsenb --rf.device_name=zmq --rf.device_args="fail_on_disconnect=true,tx_port=tcp://*:2000,rx_port=tcp://localhost:2001,id=enb,base_srate=23.04e6" &
+./srsLTE/build/srsenb/src/srsenb --rf.device_name=zmq --rf.device_args="fail_on_disconnect=true,tx_port=tcp://*:2000,rx_port=tcp://localhost:2001,id=enb,base_srate=23.04e6" &
 child=$!
 wait "$child"
